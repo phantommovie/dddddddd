f6e2b7430a1ec6628a647fe3a7087a91bb4f69d4
devilphantom <phantomdevilbot73@gmail.com>
2026-01-17 21:49:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -83,7 +83,7 @@ DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://phantombot:phantombo
 # ============================
 # Movie Notification & Update Settings
 # ============================
-MOVIE_UPDATE_NOTIFICATION = bool(environ.get('MOVIE_UPDATE_NOTIFICATION', True))  # Notification On (True) / Off (False)
+MOVIE_UPDATE_NOTIFICATION = bool(environ.get('MOVIE_UPDATE_NOTIFICATION', False))  # Notification On (True) / Off (False)
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002106653809'))  # Notification of sent to your channel
 DREAMXBOTZ_IMAGE_FETCH = bool(environ.get('DREAMXBOTZ_IMAGE_FETCH', False))  # On (True) / Off (False)
 LINK_PREVIEW = bool(environ.get('LINK_PREVIEW', False)) # Shows link preview in notification msg instead of image
